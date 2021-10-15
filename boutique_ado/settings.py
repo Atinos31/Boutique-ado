2f6765a150a1744ab3d2fa7ba460dfe1a83e0bfb
Atinos31 <atinosandra2014@gmail.com>
2021-10-15 13:13:42 +0000

    046 profile part 6 - completed confirmation email

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
 
-
     # Other
     'crispy_forms',
 ]
@@ -166,6 +165,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Stripe
@@ -175,3 +175,4 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
