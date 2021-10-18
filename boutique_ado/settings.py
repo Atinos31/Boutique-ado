20e46684df6dce98f9b28a9b479da2fd64a61eaf
Atinos31 <atinosandra2014@gmail.com>
2021-10-18 14:14:42 +0000

    b

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,7 +23,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['boutique-ado-atinos.herokuapp.com', 'localhost']
+
+ALLOWED_HOSTS = ['boutique-ado-atinos.herokuapp.com/', 'localhost']
 
 
 # Application definition
